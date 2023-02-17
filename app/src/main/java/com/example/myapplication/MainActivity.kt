0e98574f804f905e72bc88eb3abb534f44db9f1f
TomW <119408116+Murky9195@users.noreply.github.com>
2023-02-17 21:57:14 +0000

    Added a text field in which you can define a password length

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,36 +3,36 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import java.util.*
-import android.widget.Button
-import android.view.View
+import android.widget.EditText
+
 
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var passwordTextView: TextView
+    private lateinit var passwordLengthEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val generateButton = findViewById<Button>(R.id.generateButton)
-        generateButton.setOnClickListener {
-            generatePassword()
-        }
+        passwordTextView = findViewById(R.id.passwordTextView)
+        passwordLengthEditText = findViewById(R.id.passwordLengthEditText)
     }
 
+    public fun generatePassword(view: View) {
+        val passwordLength = passwordLengthEditText.text.toString().toIntOrNull() ?: 12
 
+        val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?"
+        val random = Random()
+        val password = (1..passwordLength)
+            .map { characters[random.nextInt(characters.length)] }
+            .joinToString("")
 
-
-fun generatePassword() {
-    val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?"
-    val length = 12
-    val random = Random()
-    val sb = StringBuilder(length)
-    for (i in 0 until length) {
-        sb.append(characters[random.nextInt(characters.length)])
-    }
-    val passwordTextView = findViewById<View>(android.R.id.content).findViewById<TextView>(R.id.passwordTextView)
-    passwordTextView.text = sb.toString()
+        passwordTextView.text = password
     }
 }

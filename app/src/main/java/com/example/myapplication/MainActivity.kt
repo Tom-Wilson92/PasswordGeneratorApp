63d89f5e7e3d97b232c570c7f7be2bda7b3035ea
TomW <119408116+Murky9195@users.noreply.github.com>
2023-02-18 19:03:37 +0000

    Added a copy to clipboard function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication
 
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import java.util.*
 import android.widget.EditText
+import android.widget.Toast
 import java.security.SecureRandom
 
 
@@ -36,5 +40,16 @@ class MainActivity : AppCompatActivity() {
 
         val passwordTextView = findViewById<TextView>(R.id.passwordTextView)
         passwordTextView.text = sb.toString()
+    }
+
+     fun copyToClipboard(view: View) {
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val passwordTextView = findViewById<TextView>(R.id.passwordTextView)
+
+        val password = passwordTextView.text.toString()
+        val clip = ClipData.newPlainText("password", password)
+        clipboardManager.setPrimaryClip(clip)
+
+        Toast.makeText(this, "Password copied to clipboard", Toast.LENGTH_SHORT).show()
     }
 }
